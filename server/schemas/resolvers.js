bc2ecdc8835bf6b87a978aa273d4fcf46f3678eb
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-07-17 21:26:29 +1000

    Updated user.js and resolvers and typeDef about leadboard

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,18 +16,15 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-                    .populate('questions');
-                return userData;
+                return User.findOne({ _id: context.user._id }).populate('quizzes');
             }
-            
+
             throw new AuthenticationError('Not logged in');
         }
     },
     Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create(username, email, password);
             const token = signToken(user);
 
             return { token, user };
@@ -50,7 +47,7 @@ const resolvers = {
 
             return { token, user };
         },
-        
+
         saveBook: async (parent, { input }, context) => {
             const updatedBookUser = await User.findOneAndUpdate(
                 { _id: context.user._id },
@@ -66,6 +63,31 @@ const resolvers = {
                 { new: true }
             );
             return updatedBookUser;
+        },
+        addLeaderboard: async (parent, { quizId, points }, context) => {
+            if (context.user) {
+                User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { playedQuizzes: quiz._Id } },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                )
+                return Quiz.findOneAndUpdate(
+                    { _id: quizId },
+                    {
+                        $addToSet: {
+                            Leaderboards: { points, playerId: context.user._id },
+                        },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
         },
     }
 };
