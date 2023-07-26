83dffb64e35cad22c05cba4308b967e8a143f00d
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-07-26 13:14:50 +1000

    fixed userpage

--- client/src/components/UserPageNavi.js
+++ client/src/components/UserPageNavi.js
@@ -10,24 +10,14 @@ const UserPageNavi = ({ rightLinkTo, leftLinkTo, rightTitle, leftTitle, rightSrc
         <div className="userPageNavi-container">
             <Row>
                 <Col xs={24} sm={24} md={12} lg={12} xl={12} className="creteQuizbtn-wrppaer">
-<<<<<<< HEAD
                     <Link to={leftLinkTo} ><Button className="creteQuizbtn-container">
                         {leftTitle}
-=======
-                    <Link to='/createQuiz' ><Button className="creteQuizbtn-container">
-                        Create a Quiz!
->>>>>>> main
                     </Button ></Link>
                     <div className="witch-container"><img src={leftSrc} style={{ width: "100px" }} alt={leftSrc} /></div >
                 </Col>
                 <Col xs={24} sm={24} md={12} lg={12} xl={12} className="findQuizbtn-wrapper creteQuizbtn-wrppaer">
-<<<<<<< HEAD
                     <Link to={rightLinkTo} ><Button className="findQuizbtn-container">
                         {rightTitle}
-=======
-                    <Link to='/' ><Button className="findQuizbtn-container">
-                        Play a Quiz!
->>>>>>> main
                     </Button></Link>
                     <div className="crystalBall-container witch-container"><img src={rightSrc} style={{ width: "100px" }} alt={rightSrc} /></div>
                 </Col>
